960201588b98ee947ececc84826b6b1a1734b25d
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-16 22:51:38 +0500

    if-5

--- if05.py
+++ if05.py
@@ -1,4 +1,4 @@
-def main(n):
+def main(a):
     """
     Find the largest digit of the five-digit number.
     Args:
@@ -6,4 +6,26 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x = a%10
+    y = a//10
+    z = y%10
+    e = y//10
+    b = e%10
+    d = e//10
+    f = d%10
+    g = d//10
+
+    #  x,z,b,f,g
+
+    if x>z and x>b and x>f and x>g:
+        return x
+    if z>x and z>b and z>f and z>g:
+        return z
+    if b>z and b>x and b>f and b>g:
+        return b
+    if f>z and f>b and f>x and f>g:
+        return f
+    if g>z and g>b and g>f and g>x:
+        return g
+    
+print(main(29458))
